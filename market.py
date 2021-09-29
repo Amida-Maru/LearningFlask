5582c7a511475633e140f09b2186e9798fa325a4
Amida-Maru <nippsjakub@gmail.com>
2021-09-29 23:05:35 +0100

    adding additional html templates

--- market.py
+++ market.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 #First decorator this will s
 @app.route("/home")
 @app.route("/")
-
-def hello_page():
+def home_page():
     return render_template('home.html')
 
+@app.route("/register")
+def register_page():
+    return render_template('register.html')
 
+@app.route("/login")
+def login_page():
+    return render_template('login.html')
