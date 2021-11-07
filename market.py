6151d4ab89c6ee0ecd599f37ebcdf7d2e793f132
Amida-Maru <nippsjakub@gmail.com>
2021-11-07 19:17:50 +0000

    This is an updated version of myp rogram till the before the database part

--- market.py
+++ market.py
@@ -1,17 +1,50 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
-#First decorator this will s
+
+
+items=[
+        {'id': 1, 'name': 'Phone', 'barcode': '10', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '11', 'price': 1200},
+        {'id': 3, 'name': 'Speaker', 'barcode': '12', 'price': 100},
+        {'id': 4, 'name': 'Tablet', 'barcode': '13', 'price': 300}
+    ]
+
+
+#First decorator
+
 @app.route("/home")
 @app.route("/")
 def home_page():
+
+
     return render_template('home.html')
+
+
+@app.route("/shop")
+def shop_page():
+    return render_template('shop.html', items=items)
+
+
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
+
+
+@app.route("/account")
+def account_page():
+    return render_template('account.html')
+
 
 @app.route("/register")
 def register_page():
     return render_template('register.html')
 
+
 @app.route("/login")
 def login_page():
     return render_template('login.html')
+
+
